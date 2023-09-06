110c0d5558ab473d220afbdb1aa8dfa5e253810a
Fertsi <100281472+Fertsi@users.noreply.github.com>
2023-09-06 15:41:28 +0300

    fixed search error, eliminated redundancy by trimming code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById('theatre').addEventListener('change', loadTheatre);
-
 document.getElementById('sButton').addEventListener('click', searchMovie);
 
 function loadTheatre() {
@@ -19,100 +18,34 @@ function loadTheatre() {
                     if (str.includes('TheatreID')) {
                         tID = show[i].children[a].innerHTML;
                         if (theatreId == tID) {
-                            document.getElementById("shows").innerHTML +=
-                                "<div id='frame'>" +
-                                "<div id='movie'>" +
-                                moviename(i) +
-                                "</div>" +
-                                "<div id='img'>" +
-                                "<img src='" + image(i) + "' width='100'> </img>" +
-                                "</div>" +
-                                "<div id='img2'>" +
-                                "<img src='" + rating(i) + "' width='50' id='img3'> </img>" +
-                                "</div>" +
-                                "<div id='frameTime'>" +
-                                "<div id='time'>" +
-                                "Show stars at " + startingTime(i) +
-                                "</div>" +
-                                "<div id='auditorium'>" +
-                                "Auditorium: " + auditorium(i) +
-                                "</div>" +
-                                "</div>" +
-                                "<div id='info'>" +
-                                "<a href='" + linkInfo(i) + "'> More info</a>" +
-                                "</div>" +
-                                "</div>";
+                            document.getElementById("shows").innerHTML += 
+                                "<div id='frame'>"
+                                + "<div id='movie'>" 
+                                + moviename(i)
+                                + "</div>"
+                                + "<div id='img'>" 
+                                + "<img src='" + image(i) +"' width='100'> </img>"
+                                + "</div>"
+                                + "<div id='img2'>" 
+                                + "<img src='" + rating(i) +"' width='50' id='img3'> </img>"
+                                + "</div>"
+                                + "<div id='frameTime'>"
+                                + "<div id='time'>" 
+                                + "Show starts at " + startingTime(i) 
+                                + "</div>"
+                                + "<div id='auditorium'>" 
+                                + "Auditorium: "+ auditorium(i) 
+                                + "</div>"
+                                + "</div>"
+                                + "<div id='info'>" 
+                                + "<a href='" + linkInfo(i) + "'> More info</a>" 
+                                + "</div>"
+                                + "</div>";
                         }
                     }
                 }
             }
-        } else {
-            console.error('Error loading data. Status code: ' + xhr.status);
         }
-    };
-
-    // Rest of your code here
-
-    function moviename(i) {
-        return show[i].children[15].innerHTML;
-    }
-
-    function image(i) {
-        var b = null;
-        var d = null;
-        for (a = 0; a < show[i].children.length; a++) {
-            if (show[i].children[a].nodeName.includes('Images')) {
-                b = a;
-                for (c = 0; c < show[i].children[a].children.length; c++) {
-                    if (show[i].children[a].children[c].nodeName.includes('EventLargeImagePortrait')) {
-                        d = c;
-                    }
-                }
-            }
-        }
-        return show[i].children[b].children[d].innerHTML;
-    }
-
-    function rating(i) {
-        var b = null;
-        for (a = 0; a < show[i].children.length; a++) {
-            if (show[i].children[a].nodeName.includes('RatingImageUrl')) {
-                b = a;
-            }
-        }
-        return show[i].children[b].innerHTML;
-    }
-
-    function startingTime(i) {
-        var b = null;
-        for (a = 0; a < show[i].children.length; a++) {
-            if (show[i].children[a].nodeName.includes('dttmShowStart') && !show[i].children[a].nodeName.includes('UTC')) {
-                b = a;
-            }
-        }
-        var time = show[i].children[b].innerHTML;
-        var timex = time.substr(11, 5);
-        return timex;
-    }
-
-    function auditorium(i) {
-        var b = null;
-        for (a = 0; a < show[i].children.length; a++) {
-            if (show[i].children[a].nodeName.includes('TheatreAuditorium')) {
-                b = a;
-            }
-        }
-        return show[i].children[b].innerHTML;
-    }
-
-    function linkInfo(i) {
-        var b = null;
-        for (a = 0; a < show[i].children.length; a++) {
-            if (show[i].children[a].nodeName.includes('EventURL')) {
-                b = a;
-            }
-        }
-        return show[i].children[b].innerHTML;
     }
 }
 
@@ -125,44 +58,40 @@ function searchMovie() {
             xmlDoc = xhr.responseXML;
             show = xmlDoc.getElementsByTagName('Show');
             delete1();
+            var userInput = document.getElementById('inputf').value.trim().toUpperCase();
             for (var a = 0; a < show.length; a++) {
-                for (var b = 0; b < show[a].children.length; b++) {
-                    var movieN = show[a].children[15].innerHTML;
-                    var movieNa = movieN.toUpperCase();
-                    if (movieNa.includes(document.getElementById('inputf').value.toUpperCase())) {
-                        document.getElementById("shows").innerHTML +=
-                            "<div id='frame'>" +
-                            "<div id='movie'>" +
-                            moviename(a) +
-                            "</div>" +
-                            "<div id='img'>" +
-                            "<img src='" + image(a) + "' width='100'> </img>" +
-                            "</div>" +
-                            "<div id='img2'>" +
-                            "<img src='" + rating(a) + "' width='50' id='img3'> </img>" +
-                            "</div>" +
-                            "<div id='frameTime'>" +
-                            "<div id='time'>" +
-                            "Show stars at " + startingTime(a) +
-                            "</div>" +
-                            "<div id='auditorium'>" +
-                            auditorium(a) +
-                            "</div>" +
-                            "</div>" +
-                            "<div id='info'>" +
-                            "<a href='" + linkInfo(a) + "'> More info</a>" +
-                            "</div>" +
-                            "</div>";
-                    }
+                var movieName = moviename(a).toUpperCase();
+                if (movieName.includes(userInput)) {
+                    document.getElementById("shows").innerHTML += 
+                        "<div id='frame'>"
+                        + "<div id='movie'>" 
+                        + moviename(a) 
+                        + "</div>"
+                        + "<div id='img'>" 
+                        + "<img src='" + image(a) +"' width='100'> </img>"
+                        + "</div>"
+                        + "<div id='img2'>" 
+                        + "<img src='" + rating(a) +"' width='50' id='img3'> </img>"
+                        + "</div>"
+                        + "<div id='frameTime'>"
+                        + "<div id='time'>" 
+                        + "Show starts at " + startingTime(a) 
+                        + "</div>"
+                        + "<div id='auditorium'>" 
+                        + auditorium(a) 
+                        + "</div>"
+                        + "</div>"
+                        + "<div id='info'>" 
+                        + "<a href='" + linkInfo(a) + "'> More info</a>" 
+                        + "</div>"
+                        + "</div>";
                 }
             }
-        } else {
-            console.error('Error loading data. Status code: ' + xhr.status);
         }
-    };
-
-    // Rest of your code here
+    }
 }
+
+// Rest of your code...
 
 function delete1() {
     document.getElementById("shows").innerHTML = " ";
